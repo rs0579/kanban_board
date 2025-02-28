e93524d26cfdbeb46be3a313e3fb8cd411aea361
rs0579 <raheemsenegal@outlook.com>
2025-02-28 23:33:33 +0100

    Updated the information in the authAPI on client side.

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,6 +2,24 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
+  try{
+    const response = await fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userInfo)
+    });
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error("User information is incorrect");
+    }
+    return data;
+  } catch (err) {
+    console.error('Failed to login', err);
+    return Promise.reject("Failed to login");
+  } 
 }
 
 
