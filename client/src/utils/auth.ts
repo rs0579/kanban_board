72ed64f9dc21ba1a53e302bd2990cbb3651d5c21
rs0579 <raheemsenegal@outlook.com>
2025-03-02 09:54:56 +0100

    modified the auth.ts in the utils.

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,28 +3,46 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    return jwtDecode<JwtPayload>(this.getToken());
   }
 
   loggedIn() {
-    // TODO: return a value that indicates if the user is logged in
+    // TODO: return a value that indicates if the user is lolgged in
+    const token = this.getToken();
+    // Check if the token is expired THE DOUBLE NEGATION !! IS USED TO CONVERT THE VALUE TO A BOOLEAN TO EMPHASIZE THAT THE VALUE IS A BOOLEAN
+    return !!token && !this.isTokenExpired(token);
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+  try {
+    const decoded = jwtDecode<JwtPayload>(token);
+    if (decoded?.exp && decoded?.exp < Date.now()/1000) {
+      return true;
+    }
+  } catch (error) {
+    return false;
+  }
   }
 
   getToken(): string {
     // TODO: return the token
+   const loggedUser = localStorage.getItem('token') || '';
+    return loggedUser;
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
     // TODO: redirect to the home page
+  localStorage.setItem('token', idToken);
+  window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
     // TODO: redirect to the login page
+    localStorage.removeItem('token');
+    window.location.assign('/login');
   }
 }
 
